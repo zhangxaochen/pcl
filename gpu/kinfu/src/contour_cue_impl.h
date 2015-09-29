ba01cd8646b9c961cf3899a4f30eeb8f8bbd4edb
zhangxaochen@LAB <zhangxaochen@163.com>
2015-09-30 00:09:29 +0800

    impl. *inpaintGpu* and tested it on correcteness & performance (vs. inpaintCpu)

--- gpu/kinfu/src/contour_cue_impl.h
+++ gpu/kinfu/src/contour_cue_impl.h
@@ -4,7 +4,9 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 //#include <pcl/pcl_macros.h> //与 .cu 冲突 "error C2375: 'log2f' : redefinition; different linkage"
+#include <pcl/common/time.h>
 #include <pcl/pcl_exports.h>
+
 #include "internal.h"
 // #include <pcl/gpu/containers/device_array.h>
 
@@ -12,6 +14,7 @@ using namespace std;
 
 namespace zc{
     using namespace cv;
+    using namespace pcl;
 
 //@author zhangxaochen
 //@brief implementing the INPAINTING algorithm in paper *cvpr2015-contour-cue*
@@ -35,7 +38,7 @@ PCL_EXPORTS Mat inpaintCpu(const Mat& src){
                 lbIdx = j;
                 lValidBorderVal = row[j];
 
-                //重置 rValidBorderVal：
+                //重置 rValidBorderVal： (其实未用)
                 rValidBorderVal = 0;
             }
             else if(row[j] == 0 && row[j+1] != 0){ //j无效，j+1有效
@@ -44,11 +47,11 @@ PCL_EXPORTS Mat inpaintCpu(const Mat& src){
 
                 //右边界触发修补, 向左回溯 (idx=0 极左位置无效，也能正确处理)
                 T inpaintVal = max(lValidBorderVal, rValidBorderVal);
-                for(int k = j; k > lbIdx; k--){
+                for(int k = j; k > lbIdx; k--){ //注意是 int k, 否则若 size_t k, (k>-1) 无效出错！
                     row[k] = inpaintVal;
                 }
 
-                //重置 lValidBorderVal:
+                //重置 lValidBorderVal: (有用，下面判定)
                 lValidBorderVal = 0;
             }
 
@@ -57,7 +60,7 @@ PCL_EXPORTS Mat inpaintCpu(const Mat& src){
             {
                 //此时必存在已更新但未使用的 lValidBorderVal：
                 T inpaintVal = lValidBorderVal; //== max(lValidBorderVal, rValidBorderVal); since rValidBorderVal==0
-                for(size_t k = j+1; k > lbIdx; k--){
+                for(int k = j+1; k > lbIdx; k--){
                     row[k] = inpaintVal;
                 }
             }
@@ -88,7 +91,38 @@ typedef DeviceArray2D<uchar> MaskMap;
 //@param[in] thresh, discontinunity threshold δ in paper, in (mm)
 void computeContours(const DepthMap& src, MaskMap& dst, int thresh = 50);
 
+// template<typename T>
+// void inpaintGpu(const DeviceArray2D<T>& src, DeviceArray2D<T>& dst);
+// 
+// //specialization for *ushort*
+// template<>
+// inpaintGpu<ushort>(const DeviceArray2D<ushort>& src, DeviceArray2D<ushort>& dst)
+
+//@brief similar to *inpaintCpu*, but not a func. template
 void inpaintGpu(const DepthMap& src, DepthMap& dst);
+
+namespace test{
+void testInpaintImplCpuAndGpu(const DepthMap &src, bool debugDraw = false);
+
+}//namespace test
+
+class ScopeTimeMicroSec : public StopWatch{
+    std::string title_;
+public:
+    ScopeTimeMicroSec(const char *title=""):
+      title_(title){}
+
+    ~ScopeTimeMicroSec(){
+        double val = this->getTimeMicros();
+        std::cerr << title_ << " took " << val << " micro seconds.\n";
+    }
+
+    double getTimeMicros(){
+        boost::posix_time::ptime end_time = boost::posix_time::microsec_clock::local_time();
+        return static_cast<double>((end_time - start_time_).total_microseconds());
+    }//getTimeMills
+
+};//class ScopeTimeMillis
 
 }//namespace zc
 
